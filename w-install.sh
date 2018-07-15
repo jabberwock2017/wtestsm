895a83ed55d0d27b82c1bd2b34c2e5784a87c71b
jabberwock2017 <github.com@sm.msk.ru>
2018-07-15 22:50:44 +0300

    0.0.8 fixing compose

--- w-install.sh
+++ w-install.sh
@@ -8,23 +8,22 @@ echo "/usr/bin/ssh-keygen -q -P  -f ~/.ssh/id_rsa"
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 sudo chown -R travis ~
-#echo "--ssh---"
-ssh -o StrictHostKeyChecking=no localhost ls -l /home/travis/wtestsm/wtest
-#echo "-------"
+
+ssh -o StrictHostKeyChecking=no localhost pwd
 
 sudo service docker stop
-echo "****************"
-sudo pip list
-echo "****************"
 sudo pip uninstall docker docker-py docker-compose
 sudo pip uninstall docker python
 #sudo pip install docker-py
 sudo pip install docker-compose
-sudo service docker start
 sudo pip install ansible
+sudo service docker start
+
+echo "****************"
+sudo pip list | sort
+echo "****************"
 
 sed -e "s;- set_fact: src_dir=\"{{ ansible_wd.stdout }}/wtestsm\";- set_fact: src_dir=$TRAVIS_BUILD_DIR;" <test.yml.template > t.yml
 mv t.yml test.yml.template
 
-apt list |sort
-echo "-----------------------------------------"
+( cd $TRAVIS_BUILD_DIR; docker-compose build; )
